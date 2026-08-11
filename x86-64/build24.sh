68dee6dcd0e47e2921079aeaee21057d13cf03a0
fireworld <fuxudong103@gmail.com>
2026-08-11 17:14:08 +0800

    新增一个测试pkg

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -71,6 +71,7 @@ PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
+PACKAGES="$PACKAGES homeproxy-avkiller"
 
 
 # 判断是否需要编译 Docker 插件
