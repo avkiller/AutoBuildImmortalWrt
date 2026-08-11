a069b0a53c6c282e81aa410a58233f531c54efff
fireworld <fuxudong103@gmail.com>
2026-08-11 17:33:44 +0800

    先让流程跑通

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -103,7 +103,6 @@ PACKAGES="$PACKAGES homeproxy-avkiller"
 PACKAGES="$PACKAGES dufs"
 PACKAGES="$PACKAGES luci-app-dufs"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
